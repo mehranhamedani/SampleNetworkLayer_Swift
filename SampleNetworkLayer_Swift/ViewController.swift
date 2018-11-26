dc3307dcfcfc0e486dd84e5e2c55f4a37da69fa1
Mehran Hamedani Nezhad <mehran@mehrans-mac.local>
2018-11-26 20:01:32 +0330

    Initial Commit

---
+++ SampleNetworkLayer_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleNetworkLayer_Swift
+//
+//  Created by Mehran Hamedani Nezhad on 11/26/18.
+//  Copyright © 2018 BehcomStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
